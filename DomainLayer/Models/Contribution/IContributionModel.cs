dba4e526250303b5ab06231580ef502c0165d466
AJ-Sabado <a.sabado.522151@umindanao.edu.ph>
2025-03-18 20:59:19 +0800

    Extracted Contributions from Salary

---
+++ DomainLayer/Models/Contribution/IContributionModel.cs
@@ -0,0 +1,15 @@
+﻿using DomainLayer.Models.Employee;
+
+namespace DomainLayer.Models.Contribution
+{
+    public interface IContributionModel
+    {
+        EmployeeModel Employee { get; set; }
+        Guid EmployeeId { get; set; }
+        Guid Id { get; set; }
+        decimal PagIbigAmount { get; set; }
+        decimal PhilHealthAmount { get; set; }
+        decimal SSSAmount { get; set; }
+        decimal TotalContributions { get; set; }
+    }
+}
