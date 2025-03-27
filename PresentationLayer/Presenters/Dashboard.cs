7d70e093839323b9a3f64a521523852b87afb5c9
AJ-Sabado <a.sabado.522151@umindanao.edu.ph>
2025-03-27 14:10:04 +0800

    Fixed Errors in Dashboard Presenter

--- PresentationLayer/Presenters/Dashboard.cs
+++ PresentationLayer/Presenters/Dashboard.cs
@@ -21,57 +21,15 @@ namespace PresentationLayer.Presenters
         }
         public DashboardDetailsViewModel GetDashboardDetails(IUserModel user)
         {
-            var employee = _unitOfWork.EmployeeRepository.GetById(user.EmployeeId); // diri
-            var salaryInfo = _unitOfWork.SalaryRepository.GetByEmployee(user.EmployeeId); // diri
-
-            return new DashboardDetailsViewModel
-            {
-                NetSalary = salaryInfo.NetSalary, // dirii
-                Allowance = salaryInfo.Allowance,
-                Bonuses = salaryInfo.Bonuses,
-                Deductions = salaryInfo.Deductions,
-                Absences = employee.Absences //dirii
-            };
+            return _unitOfWork.GetDashboardDetails(user);
         }
         public IEnumerable<AttendanceLogViewModel> GetAttendanceLogs(IUserModel user)
         {
-            return _unitOfWork.AttendanceRepository
-            .GetForEmployee(user.EmployeeId) // dunno unsa error
-            .Select(a => new AttendanceLogViewModel
-            {
-                Date = a.Date,
-                TimeIn = a.TimeIn,
-                TimeOut = a.TimeOut,
-                Status = a.Status
-            });
+            return _unitOfWork.GetAttendanceLog(user);
         }
         public JobDeskDetailsViewModel GetJobDeskDetails(IUserModel user)
         {
-            var employee = _unitOfWork.EmployeeRepository.GetById(user.EmployeeId);
-
-            return new JobDeskDetailsViewModel
-            {
-                Department = employee.Department,
-                BaseSalary = employee.BaseSalary,
-                WorkSlot = employee.WorkSlot,// paedit ko diri jay
-                EmploymentStatus = employee.EmploymentStatus,
-                EmploymentDate = employee.EmploymentDate,
-                Contacts = new ContactsViewModel // ug diri
-                {
-                    Email = employee.Email,
-                    Phone = employee.Phone,
-                    Website = employee.Website
-                },
-                Payslip = new PayslipViewModel // diri saddd
-                {
-                    BaseSalary = employee.BaseSalary,
-                    Bonuses = employee.Bonuses,
-                    Deductions = employee.Deductions,
-                    Total = employee.NetSalary,
-                    PayrollDate = employee.LastPayrollDate,
-                    Status = "Completed"
-                }
-            };
+            return _unitOfWork.GetJobDeskDetails(user);
         }
     }
 }
