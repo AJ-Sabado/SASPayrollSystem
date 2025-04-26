b2849d909a8f4d4a274737a1a2db31d81ba7c60d
TimRobin10 <timrobinremeticado@gmail.com>
2025-04-26 19:30:12 +0800

    UI Refact

--- PresentationLayer.WPF/View/Pages/Dashboard/EmployeeDashboardReg/RegJobDesk.xaml.cs
+++ PresentationLayer.WPF/View/Pages/Dashboard/EmployeeDashboardReg/RegJobDesk.xaml.cs
@@ -1,28 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using PresentationLayer.WPF.ViewModel.RegularViewModel;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PresentationLayer.WPF.View.Pages.Dashboard.EmployeeDashboardReg
 {
-    /// <summary>
-    /// Interaction logic for RegJobDesk.xaml
-    /// </summary>
     public partial class RegJobDesk : UserControl
     {
         public RegJobDesk()
         {
             InitializeComponent();
+            DataContext = new RegJobDesk_ViewModel();
         }
     }
 }
---
+++ PresentationLayer.WPF/ViewModel/RegularViewModel/RegJobDesk_ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using PresentationLayer.WPF.View.Windows;
+using System.Windows.Input;
+
+namespace PresentationLayer.WPF.ViewModel.RegularViewModel
+{
+    public class RegJobDesk_ViewModel
+    {
+        public ICommand FileLeaveCommand { get; }
+
+        public RegJobDesk_ViewModel()
+        {
+            FileLeaveCommand = new RelayCommand(FileLeave);
+        }
+
+        //FUNCTIONS
+
+        //LEAVE FORM FUNCTIONS
+        private void FileLeave(object? obj)
+        {
+            var _fileLeaveDialog = new FileLeaveForm_View();
+            _fileLeaveDialog.ShowDialog();
+        }
+    }
+}
