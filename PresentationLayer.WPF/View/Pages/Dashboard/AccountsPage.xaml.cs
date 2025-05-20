b68bd68fd606bb3b35dd54021d6f0d9a58b9a461
Aj-Sabado <a.sabado.522151@umindanao.edu.ph>
2025-05-20 14:58:18 +0800

    Started change password

--- PresentationLayer.WPF/View/Pages/Dashboard/AccountsPage.xaml.cs
+++ PresentationLayer.WPF/View/Pages/Dashboard/AccountsPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using PresentationLayer.WPF.ViewModel.PagesViewModel;
 using System.Windows.Controls;
 using DomainLayer.Enums.EmployeePersonalInfo;
+using System.Windows;
 
 namespace PresentationLayer.WPF.View.Pages.Dashboard
 {
@@ -13,6 +14,25 @@ namespace PresentationLayer.WPF.View.Pages.Dashboard
         {
             InitializeComponent();
             DataContext = vm;
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            var passwordBox = sender as PasswordBox;
+            if (passwordBox != null)
+            {
+                string password = passwordBox.Password;
+                SendPasswordToViewModel(password);
+            }
+        }
+
+        //Code-behind pasword binding
+        private void SendPasswordToViewModel(string password)
+        {
+            if (DataContext is AccountPage_ViewModel viewModel)
+            {
+                viewModel.Password = password;
+            }
         }
     }
 }
