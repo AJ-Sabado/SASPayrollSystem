9a8806151271876bd14d0ab7bd82014091228429
nrnrlds <n.rulida.533869@umindanao.edu.ph>
2025-05-21 19:18:01 +0800

    Add ConnectionString

--- InfrastructureLayer/DataAccess/AppDbContext.cs
+++ InfrastructureLayer/DataAccess/AppDbContext.cs
@@ -15,9 +15,10 @@ namespace InfrastructureLayer.DataAccess
         private const string connectionStringLab = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SASPayrollDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
         private const string connectionStringTim = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SASPayrollDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
         private const string connectionStringAFA = "Data Source=(localdb)\\ProjectModels;Initial Catalog=SASPayrollDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        private const string connectionStringNoreen = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SASPayrollDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(connectionStringAFA);
+            optionsBuilder.UseSqlServer(connectionStringNoreen);
             base.OnConfiguring(optionsBuilder);
         }
 
