2ed0f5ef4cd1316682ddfa98446209ed5afa702b
Aj-Sabado <a.sabado.522151@umindanao.edu.ph>
2025-05-23 19:22:25 +0800

    Window memoey leak possibly fixed. Please test.

--- PresentationLayer.WPF/ViewModel/RegularViewModel/LoginPage_ViewModel.cs
+++ PresentationLayer.WPF/ViewModel/RegularViewModel/LoginPage_ViewModel.cs
@@ -131,8 +131,8 @@ namespace PresentationLayer.WPF.ViewModel.RegularViewModel
                     //Test attendance evaluation
                     try
                     {
-                        await _unitOfWork.EvaluateAllEmployeeAttendanceLog(periodStart, periodEnd);
-                        await _unitOfWork.GenerateAllEmployeePayslips(periodStart, periodEnd, payDate);
+                        //await _unitOfWork.EvaluateAllEmployeeAttendanceLog(periodStart, periodEnd);
+                        //await _unitOfWork.GenerateAllEmployeePayslips(periodStart, periodEnd, payDate);
                         _windowService.ShowWindow<EmployeeDahboard_View>();
                     }
                     catch (Exception ex)
@@ -146,7 +146,7 @@ namespace PresentationLayer.WPF.ViewModel.RegularViewModel
                 {
                     try
                     {
-                        //await _unitOfWork.GenerateAllContractorPayslips(periodStart, periodEnd, payDate);
+                        await _unitOfWork.GenerateAllContractorPayslips(periodStart, periodEnd, payDate);
                         _windowService.ShowWindow<EmployeeDashboardIC_View>();
                     }
                     catch (Exception ex)
