9df7b2f9da0f286939cd799597f81b96928e53e0
TimRobin10 <timrobinremeticado@gmail.com>
2025-05-23 13:02:23 +0800

    WORKFORCE LOGS PAGES DONE

---
+++ PresentationLayer.WPF/View/Pages/Dashboard/AdminDashboard/AdminWorkforce_View.xaml.cs
@@ -0,0 +1,17 @@
+﻿using PresentationLayer.WPF.ViewModel.PagesViewModel.AdminDashboard;
+using System.Windows.Controls;
+
+namespace PresentationLayer.WPF.View.Pages.Dashboard.AdminDashboard
+{
+    /// <summary>
+    /// Interaction logic for AdminWorkforce_View.xaml
+    /// </summary>
+    public partial class AdminWorkforce_View : UserControl
+    {
+        public AdminWorkforce_View(AdminWorkforce_ViewModel vm)
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+    }
+}
